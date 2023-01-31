38a8cd9e73de396bd565d4e9a3c60e855668629f
Sergey Kuzin <105937658+SerjKenri@users.noreply.github.com>
2023-01-31 16:35:36 +0200

    Refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,72 @@
-export const App = () => {
+import React, { useState, useEffect } from "react";
+import { nanoid } from 'nanoid'
+import { Section } from "./Section/Section";
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
+import { Filter } from "./Filter/Filter";
+import defaultContacts from "./DefaultContacts/defaultContacts";
+
+
+
+export function App () {
+  const [contacts , setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contact')) ?? defaultContacts;
+  });
+  const [filtered, setFiltered] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contact', JSON.stringify(contacts));
+  },[contacts])
+
+
+  const handleSubmit = e => {
+    const id = nanoid(5);
+    const name = e.name;
+    const number = e.number;
+    const contactsLists = [...contacts];
+
+    if (contactsLists.findIndex(contact => name === contact.name) !== -1) {
+      alert(`${name} is already in contacts.`);
+    } else {
+      contactsLists.push({ id, name, number });
+    }
+
+    setContacts({ contactsLists });
+  };
+
+  const handleDelete = (id) => {
+    setContacts(prev => prev.filter(contacts => contacts.id !== id));
+  };
+
+  const handleChange = e => {
+    setFiltered(e.currentTarget.value)
+  };
+
+  const handleFilter = () => {
+    const filterContactsList = filtered.toLowerCase();
+    return contacts.filter(({ name }) => 
+    name.toLowerCase().includes(filterContactsList));
+  };
+  
+
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 25,
         color: '#010101'
       }}
     >
-      React homework template
+      <Section title="Phonebook">
+        <ContactForm onSubmit={handleSubmit}/>
+      </Section>
+
+      <Section title="Contacts">
+        <Filter filter={filtered} onChange={handleChange}/>
+        <ContactList contacts={handleFilter()} onDelete={handleDelete}/>
+      </Section>
     </div>
   );
 };
